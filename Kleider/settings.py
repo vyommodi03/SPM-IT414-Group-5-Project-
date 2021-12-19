0c37b8bceb07c8d46fcf38bb78dbc59988a9010b
Vyom <vyommodi03@gmail.com>
2021-12-19 14:15:19 +0530

    changes

--- Kleider/settings.py
+++ Kleider/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*^e27bxup%s2@p8fjj126%v8p=g%d6o^#=&4^kr-mauf3^^&c_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
